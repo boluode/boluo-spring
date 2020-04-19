5065d2471520f470776d8831c1c070a1a648c651
菠萝 <liyanlong1@100tal.com>
2020-04-19 20:57:54 +0800

    Spring Scope

--- src/main/java/com/boluo/spring/config/ScopeConfig.java
+++ src/main/java/com/boluo/spring/config/ScopeConfig.java
@@ -1,10 +1,9 @@
 package com.boluo.spring.config;
 
+import com.boluo.spring.condition.LinuxCondition;
+import com.boluo.spring.condition.MacCondition;
 import com.boluo.spring.model.Person;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class ScopeConfig {
@@ -16,11 +15,26 @@ public class ScopeConfig {
      * request: 同一个请求 一个实例
      * session: 同一个session 一个实例
      */
-//    @Scope("prototype")
-    @Lazy
+    @Scope("prototype")
+//    @Lazy
     @Bean
     public Person person() {
         System.out.println("给容器中添加Person...");
         return new Person("菠萝", 18);
+    }
+
+    /**
+     * Conditional
+     */
+    @Conditional({MacCondition.class})
+    @Bean
+    public Person mac() {
+        return new Person("mac", 29);
+    }
+
+    @Conditional({LinuxCondition.class})
+    @Bean
+    public Person linux() {
+        return new Person("linux", 18);
     }
 }
--- src/test/java/com/boluo/spring/config/ScopeConfigTest.java
+++ src/test/java/com/boluo/spring/config/ScopeConfigTest.java
@@ -1,22 +1,48 @@
 package com.boluo.spring.config;
 
 import com.boluo.spring.model.Person;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class ScopeConfigTest {
 
+    private AnnotationConfigApplicationContext context;
+
+    @Before
+    public void init() {
+        context = new AnnotationConfigApplicationContext(ScopeConfig.class);
+    }
+
     @Test
     public void test() {
-
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class);
 
         System.out.println("IOC容器创建完成....");
         Person person1 = (Person) context.getBean("person");
         Person person2 = (Person) context.getBean("person");
         System.out.println(person1 == person2);
         context.close();
+    }
+
+    @Test
+    public void testCondition() {
+
+        System.out.println("IOC容器创建完成....");
+
+        ConfigurableEnvironment environment = context.getEnvironment();
+        System.out.println("当前操作系统的名称" + environment.getProperty("os.name"));
+
+        String[] beanNamesForType = context.getBeanNamesForType(Person.class);
+        for (String s : beanNamesForType) {
+            System.out.println(s);
+        }
+        System.out.println("***************************************");
+        Map<String, Person> beansOfType = context.getBeansOfType(Person.class);
+        System.out.println(beansOfType);
     }
 }
