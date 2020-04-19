1a825e14e3a950a2702d6a807d4dd038e31787de
菠萝 <liyanlong1@100tal.com>
2020-04-19 20:58:15 +0800

    Spring Conditional

---
+++ src/main/java/com/boluo/spring/condition/LinuxCondition.java
@@ -0,0 +1,13 @@
+package com.boluo.spring.condition;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class LinuxCondition implements Condition {
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+
+
+        return false;
+    }
+}
